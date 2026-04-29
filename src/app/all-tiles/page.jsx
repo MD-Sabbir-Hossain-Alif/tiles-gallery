33a9b3af0e9935d88ad1f33abfc6f6c413ef490f
MD-Sabbir-Hossain-Alif <sabbir.web.dp@gmail.com>
2026-04-30 04:26:49 +0600

    all route created

---
+++ src/app/all-tiles/page.jsx
@@ -0,0 +1,5 @@
+const AllTilesPage = () => {
+    return <div>All Tiles Page</div>;
+};
+
+export default AllTilesPage;
---
+++ src/app/tile/[id]/page.jsx
@@ -0,0 +1,5 @@
+const TileDetailsPage = () => {
+    return <div>Tile Details Page</div>;
+};
+
+export default TileDetailsPage;
